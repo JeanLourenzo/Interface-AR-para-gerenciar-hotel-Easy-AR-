e8a671e4868806019ad588592396d2098c28e3f3
LeanLourenzo <lean.lourenzo@live.com>
2019-05-02 17:57:43 -0300

    Interface hexagonal tentativa

---
+++ Assets/EasyAR/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+  
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);        
+    }
+
+    public void UnityQuit(){
+        Application.Quit();
+    }
+
+    public void Voltar(){
+
+    SceneManager.LoadScene(0);        
+        
+    }
+}
---
+++ Assets/WebsiteButton.cs
@@ -0,0 +1,28 @@
+﻿     using System.Collections;
+     using System.Collections.Generic;
+     using UnityEngine;
+     using UnityEngine.Events;
+     
+     public class WebsiteButton : MonoBehaviour {
+     
+         public GameObject definedButton;
+         public UnityEvent OnClick = new UnityEvent();
+     
+         void Start () {
+             definedButton = this.gameObject;
+         }
+         
+         void Update () {
+             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             RaycastHit Hit;
+             
+             if (Input.GetMouseButtonDown(0))
+             {
+                 if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+                 {
+                     Debug.Log("Button Clicked");
+                     OnClick.Invoke();
+                 }
+             }    
+         }
+     }
