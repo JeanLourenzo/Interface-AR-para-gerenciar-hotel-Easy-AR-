d05e5ad87e7dfa43175019fe02be2e3211aa2976
LeanLourenzo <lean.lourenzo@live.com>
2019-05-04 17:59:36 -0300

    Interface com painel, tentando button hexagonal

---
+++ Assets/CriaOnclickEmObjeto.cs
@@ -0,0 +1,25 @@
+﻿     using System.Collections;
+     using System.Collections.Generic;
+     using UnityEngine;
+     using UnityEngine.Events;
+     
+     public class CriaOnclickEmObjeto : MonoBehaviour {
+     
+         public UnityEvent OnClick = new UnityEvent();
+         
+         void Update ()
+        {
+             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             RaycastHit Hit;
+             
+             if (Input.GetMouseButtonDown(0))
+             {
+                if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+                {
+                  Debug.Log("Funcionou");
+                  OnClick.Invoke();
+               
+                }
+             }
+        }
+     }
---
+++ Assets/MudarTexto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MudarTexto : MonoBehaviour
+{
+    public Text TextoMostrado = null;
+    
+    public void MudaTexto(string novotexto)
+    {
+    novotexto = "BBB";
+    TextoMostrado.text = novotexto;
+
+        
+    }
+}
